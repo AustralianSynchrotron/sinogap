e6399697d62c2d0cad88a19a0ce9035ec3f94bb2
Your Name <you@example.com>
2025-02-17 11:42:32 +1100

    modified:   dupas.sh 	modified:   sinogap_bp0.ipynb 	modified:   sinogap_bp1.ipynb 	modified:   sinogap_bp2.ipynb 	modified:   sinogap_bp3.ipynb

--- dupas.sh
+++ dupas.sh
@@ -14,11 +14,11 @@ fi
 devNum="${2}"
 
 rm -rf runs/experiment_$devNum/
-cp -r ${iDir}/logs runs/experiment_$devNum
-cp ${iDir}/checkPoint.pth checkPoint_${devNum}.pth 
-cp ${iDir}/train.hdf train_${devNum}.hdf 
-cp ${iDir}/sinogap.ipynb sinogap_${devNum}.ipynb
-cp ${iDir}/model_gen.pt model_${devNum}_gen.pt
+cp -v -r ${iDir}/logs runs/experiment_$devNum
+cp -v ${iDir}/checkPoint.pth checkPoint_${devNum}.pth 
+cp -v ${iDir}/train.hdf train_${devNum}.hdf 
+cp -v ${iDir}/sinogap.ipynb sinogap_${devNum}.ipynb
+cp -v ${iDir}/model_gen.pt model_${devNum}_gen.pt
 
 
 
